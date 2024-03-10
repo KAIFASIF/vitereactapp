4f328272522a7e58dbf4b4381cdc91e9a4df9186
KAIFASIF <89296630+KAIFASIF@users.noreply.github.com>
2024-03-10 21:23:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <p>
-       Testing my first app on aws ec2 using github docker and jenkins pipeline and addeded new content
+        Its ajenkins pipeline  with docker  on ec2
       </p>
       <p>
         Click on the Vite and ddddddddddddddddddddddddReact logos to learn more
